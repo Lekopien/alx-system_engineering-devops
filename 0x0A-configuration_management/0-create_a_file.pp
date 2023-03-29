8527a76d4c2276dd8258aa895fe6f71aeb10c9a9
Lekopien <51331171+Lekopien@users.noreply.github.com>
2023-03-29 12:21:21 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file in /tmp
 
-file { '/tmp/holberton':
+file { '/tmp/School':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/School',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
